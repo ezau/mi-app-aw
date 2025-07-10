ca848e825e6974881daf5d8d19649900e39bb783
Ezau Jiménez Valdez <ezau.jimenez@ejemplo.com>
2025-07-10 01:35:28 -0600

    Actualización de la interfaz

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>¡Mi App en AWS Amplify!</h1>
-        <p>Despliegue continuo con GitHub</p>
-        <small>Versión 1.0</small>
+       <h1>¡Mi App en AWS Amplify con CI/CD!</h1>
+<p>Actualización automática desde GitHub</p>
+<small>Versión 1.1</small>
       </header>
     </div>
   );
